84d13ca7c40075f7d0dc9161f0f0cc4c42885f9e
pokerdio <diodor.bitan@gmail.com>
2026-07-04 16:28:07 +0300

    space

--- p5js/space/block.js
+++ p5js/space/block.js
@@ -10,17 +10,43 @@ class Block {
     constructor() {
 	this.x = random(width);
 	this.y = 0;
-	this.size = random(config.max_size / 4.0, 
-			   config.max_size)
+	let f = random();
+	f = f * f;
+	this.size = config.max_size * config.small_frac +
+	    f * (1 - config.small_frac) * config.max_size;
+
+	this.vx = (random() * 2.0 - 1.0) * config.blockv;
+	this.vy = (random() * 2.0 - 1.0) * config.blockv;
+	
+	this.color = config.block_col
+	[Math.floor(random() * config.block_col.length)];
     }
 
     display() {
+	this.x += this.vx;
+	this.y += this.vy;
+
 	push();
 	translate(this.x, this.y);
-	strokeWeight(2.5);
-	stroke(255, 0, 100);
-	fill(0, 50, 75);
-	circle(0, 0, this.size);
+	strokeWeight(1.5);
+	stroke(0, 0, 0);
+	fill(this.color);
+	circle(0, 0, this.size * 2);
+	strokeWeight(0);
+	fill("black");
+	circle(-this.size*0.4, -this.size*0.4, 
+	       this.size * 0.5);
+	fill("white");
+	circle(-this.size*0.5, -this.size*0.5, 
+	       this.size * 0.3);
+
+	fill("black");
+	circle(this.size*0.6, -this.size*0.4, 
+	       this.size * 0.5);
+	fill("white");
+	circle(this.size*0.5, -this.size*0.5, 
+	       this.size * 0.3);
+
 	pop();
     }
 
--- p5js/space/sketch.js
+++ p5js/space/sketch.js
@@ -1,49 +1,137 @@
 let cols; let rows;
 let size = 15;
+let config = {spawn_chance: 5.0, 
+	      spawn_chance_extra:25.0,
+	      spawn_max_age:15000.0,
+	      dy: 3.5, max_size: 30,
+	      canvasX: 600, canvasY: 400, mouse_approach: 0.1,
+	      small_frac: 0.125, blockv: 0.5,
+	      block_col: [[55,0,0], 
+			  [0,55,0], 
+			  [0,0,55], 
+			  [55,0,55], 
+			  [55,55,0], ]};
+
+let world = {
+    age: 0, 
+    blocks: [],
+    playerX: 10,
+    playerY: config.canvasY - 10,
+    playerX_target: 10,
+    dead: false
+}
 let blocks = [];
-let config = {spawn_chance: 10.0, dy: 3.5, max_size: 66};
 
 function setup() {
-    createCanvas(405, 405);
+    createCanvas(config.canvasX, config.canvasY);
     rectMode(CENTER);
     angleMode(DEGREES);
 }
 
 function mouseMoved() {
-    if ((mouseX < 0) || (mouseX > width) || (mouseX < 0) || (mouseX > width)) {
+    world.playerX_target = mouseX;
+    if ((mouseX < 0) || (mouseX > config.canvasX) || 
+	(mouseY < 0) || (mouseY > config.canvasY)) {
 	cursor(ARROW);
+	if (mouseX < 0) {
+	    world.playerX_target = 0;
+	}	
+	if (mouseX > config.canvasX) {
+	    world.playerX_target = config.canvasX;
+	}
     } else {
 	noCursor();
     }
+
+
+    world.playerX += (world.playerX_target - world.playerX) * 
+	config.mouse_approach;
 }
 
-function draw() {
-    background(220);
-    if (random (100) < config.spawn_chance) {
-	blocks.push(new Block())
+function mouseClicked () {
+    world.dead = false;
+    world.blocks = [];
+    world.age = 0;
+}
+
+function fadeOut () {
+    fill (0, 0, 0, 15);
+    rect (config.canvasX / 2, config.canvasY / 2,
+	  config.canvasX, config.canvasY);
+}
+
+function advance_blocks () {
+    let chance = config.spawn_chance;
+    if (world.age > config.spawn_max_age) {
+	chance += config.spawn_chance_extra
+    } else {
+	chance += config.spawn_chance_extra * world.age / 
+	    config.spawn_max_age;
     }
-    if (mouseX != pmouseX) {
-	console.log("WAAAAAAAA")
+
+    fill("black");
+    textSize(22);
+    text(Math.floor(chance * 4) * 0.25, 50, 50);
+
+    if (random (100) < chance) {
+	world.blocks.push(new Block());
     }
-    for (let i=0 ; i<blocks.length ; ++i) {
-	blocks[i].move();
+    for (let i=0 ; i<world.blocks.length ; ++i) {
+	world.blocks[i].move();
     }
-    for (let i=0 ; i<blocks.length ; ++i) {
-	blocks[i].display();
+    for (let i=0 ; i<world.blocks.length ; ++i) {
+	world.blocks[i].display();
     }
     new_blocks = [];
-    for (let i=0 ; i<blocks.length ; ++i) {
-	if (blocks[i].y - blocks[i].size < height) {
-	    new_blocks.push(blocks[i])
+    for (let i=0 ; i<world.blocks.length ; ++i) {
+	if (world.blocks[i].y - world.blocks[i].size < height) {
+	    new_blocks.push(world.blocks[i])
 	}
     }
-    blocks = new_blocks;
+    world.blocks = new_blocks;
+}
 
+function advance_player() {
     push();
-    translate( mouseX, height - 10);
+    translate( world.playerX, world.playerY);
     strokeWeight(1.5);
     stroke(0, 0, 0);
     fill(255, 100, 100);
     triangle(0, -10, -10, 0, 10, 0);
     pop();
+}
+
+function dist (x1, y1, x2, y2)  {
+    let dx = abs(x1 - x2);
+    let dy = abs(y1 - y2);
+    return sqrt(dx * dx + dy * dy);
+}
+
+function collision_detect() {
+    triangle();
+    for ([dx, dy] of [[0, -10], [-10, 0], [10, 0]]) {
+	for (let i=0 ; i<world.blocks.length ; ++i) {
+	    let b = world.blocks[i];
+	    let d = dist(b.x, b.y, 
+			 world.playerX + dx, 
+			 world.playerY + dy);
+	    if (d < b.size) {
+		world.dead = true;
+		return;
+	    }
+	}
+    }
+}
+
+function draw() {
+    if (world.dead) {
+	fadeOut();
+	return;
+    }
+    world.age += 1; 
+    background(220);
+
+    advance_blocks();
+    advance_player();
+    collision_detect();
 }
